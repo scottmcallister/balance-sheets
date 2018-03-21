1d0ae814e0532325baca6f52338df26e99fc77a7
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-03-20 21:41:07 -0300

    remove UI related files from scala API

--- server/app/controllers/HomeController.scala
+++ server/app/controllers/HomeController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.libs.json.JsObject
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -12,13 +13,11 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page.
-   *
    * The configuration in the `routes` file means that this method
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok(JsObject(Seq.empty))
   }
 }
