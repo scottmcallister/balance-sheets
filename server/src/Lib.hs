ffd2e87ebaa671a020d4c9676d1cbd1926a34041
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-03-24 16:24:31 -0300

    add servant API

---
+++ server/src/Lib.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators   #-}
+module Lib
+    ( startApp
+    , app
+    ) where
+
+import Data.Aeson
+import Data.Aeson.TH
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+
+data User = User
+  { userId        :: Int
+  , userFirstName :: String
+  , userLastName  :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''User)
+
+type API = "users" :> Get '[JSON] [User]
+
+startApp :: IO ()
+startApp = do
+  putStrLn "app running on localhost:8080"
+  run 8080 app
+
+app :: Application
+app = serve api server
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server = return users
+
+users :: [User]
+users = [ User 1 "Scott" "McAllister"
+        , User 2 "Kelly" "McAllister"
+        ]
