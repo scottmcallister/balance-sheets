006578202991ecc5f90e7a313437bf252e979632
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-03-25 16:41:48 -0300

    enable cors

--- server/src/Lib.hs
+++ server/src/Lib.hs
@@ -11,6 +11,7 @@ import Data.Aeson
 import Data.Aeson.TH
 import Network.Wai
 import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.Cors
 import Servant
 
 data User = User
@@ -22,11 +23,6 @@ data User = User
 $(deriveJSON defaultOptions ''User)
 
 type API = "users" :> Get '[JSON] [User]
-
-startApp :: IO ()
-startApp = do
-  putStrLn "app running on localhost:8080"
-  run 8080 app
 
 app :: Application
 app = serve api server
@@ -41,3 +37,26 @@ users :: [User]
 users = [ User 1 "Scott" "McAllister"
         , User 2 "Kelly" "McAllister"
         ]
+
+
+userResourcePolicy :: CorsResourcePolicy
+userResourcePolicy =
+  CorsResourcePolicy
+    { corsOrigins = Nothing
+    , corsMethods = simpleMethods
+    , corsRequestHeaders = simpleHeaders
+    , corsExposedHeaders = Nothing
+    , corsMaxAge = Nothing
+    , corsVaryOrigin = False
+    , corsRequireOrigin = False
+    , corsIgnoreFailures = False
+    }
+
+usersCors :: Middleware
+usersCors = cors $ const (Just userResourcePolicy)
+
+
+startApp :: IO ()
+startApp = do
+  putStrLn "app running on localhost:8080"
+  run 8080 $ usersCors app
